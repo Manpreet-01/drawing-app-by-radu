9d551805149b60f5b3684cf8dc3ae6d61e44cdaa
Manpreet Singh <manpreetmss400@gmail.com>
2023-06-29 22:02:27 +0530

    undo btn implemented

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -7,12 +7,20 @@ class SketchPad{
 			background-color: white;
 			box-shadow: 0px 0px 10px 2px black;
 		`;
-		
 		container.appendChild(this.canvas);
+		
+		const lineBreak = document.createElement("br");
+		container.appendChild(lineBreak);
+		
+		this.undoBtn=document.createElement("button");
+		this.undoBtn.innerHTML="UNDO";
+		container.appendChild(this.undoBtn);
+		
 		this.ctx = this.canvas.getContext("2d");
 		
 		this.paths=[];
 		this.isDrawing=false;
+		this.#redraw();
 		
 		this.#addEventListeners(); //private method
 	}
@@ -47,10 +55,20 @@ class SketchPad{
 		this.canvas.ontouchend=(evt)=>{
 			this.canvas.onmouseup();
 		}
+		this.undoBtn.onclick=()=>{
+			this.paths.pop();
+			this.#redraw();
+		}
 	}
 	#redraw(){
 		this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 		draw.paths(this.ctx, this.paths);
+		if(this.paths.length>0){
+			this.undoBtn.disabled = false;
+		}else{
+			this.undoBtn.disabled = true;
+			
+		}
 	}
 	#getMouse=(evt)=>{
 		const rect = this.canvas.getBoundingClientRect();
