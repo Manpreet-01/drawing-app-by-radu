1296a3f7680ad7cacd66f039dc1112b413781744
Manpreet Singh <manpreetmss400@gmail.com>
2023-06-30 21:56:57 +0530

    add some improvements

--- js/display.js
+++ js/display.js
@@ -35,17 +35,33 @@ function createRow(container, studentName, samples){
 }
 
 function handleClick(sample, doScroll=true){
-	[...document.querySelectorAll(".emphasize")].
-		forEach((e)=>e.classList.remove('emphasize'));
+	if(sample==null){
+		[...document.querySelectorAll(".emphasize")].
+			forEach((e)=>e.classList.remove('emphasize'));
+		return;
+	}
+	
 	const el = document.getElementById(
 		"sample_"+sample.id
 	);
+	
+	if(el.classList.contains("emphasize")){
+		el.classList.remove("emphasize");
+		chart.selectSample(null);
+		return;
+	}
+	
+	// [...document.querySelectorAll(".emphasize")].
+		// forEach((e)=>e.classList.remove('emphasize'));
+			
 	el.classList.add("emphasize");
+	
 	if(doScroll){
 		el.scrollIntoView({
 			behavior : 'auto',
 			block: 'center'
 		});
 	}
+	
 	chart.selectSample(sample);
 }
